08058ae0e17547f90e0eb350e978d556e29e5b52
Alessandro Canevaro <s212977@dtu.dk>
2022-02-25 15:02:03 +0100

    AI_agent class outline

---
+++ AI_agent.py
@@ -0,0 +1,15 @@
+class Agent:
+    def __init__(self) -> None:
+        pass
+
+    def ExpandTree(self) -> None:
+        pass
+
+    def ComputeHeuristics(self) -> None:
+        pass
+
+    def ComputeNextMove(self) -> int:
+        return 0
+
+if __name__ == "__main__":
+    print(Do some tests...)

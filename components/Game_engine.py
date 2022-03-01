0f70476cd3bbce0df65486b99b95b22172c1e22c
Alessandro Canevaro <s212977@dtu.dk>
2022-03-01 15:43:53 +0100

    swipeleft v1

--- components/Game_engine.py
+++ components/Game_engine.py
@@ -1,6 +1,6 @@
 
 import random
-import numpy as npy
+import numpy as np
 import copy as cp
 
 board_size = 4
@@ -8,29 +8,48 @@ temp_board =[]
 class GameEngine:
     def __init__(self):
         #self.board = [[0 for i in range[4]] for j in range [4]]
-        self.board = npy.zeros((board_size,board_size))
+        self.board = np.zeros((board_size,board_size))
 
     def setRandomNumberInTile(self, k=1) ->None: 
         #Trying to get position of the board in the form of (x,y)
         #Here the k will decide how many positions to put random number
-        board_position = list(zip(*npy.where(self.board == 0)))
+        board_position = list(zip(*np.where(self.board == 0)))
         for rand_pos in random.sample(board_position, k=k):
             if random.randint(1, 8) == 1:
                 self.board[rand_pos] = 4
             else:
                 self.board[rand_pos] = 2
-    #Swipe left        
-    def swipeLeft(self,move):
-        for i in range(board_size):
-            zvalue =  self.board[i, :]
-            nvalue = zvalue[zvalue !=0]   
-            fvalue = npy.zeros_like(zvalue)
-            fvalue[:len(nvalue)] = nvalue
-            self.board[i, :] = fvalue
-            temp_board = cp.deepcopy(self.board)
+
+    def _compress(self, row):
+        """compress all the numbers on one side of the board.
+        """
+        new_row = [i for i in row if i != 0]
+        return new_row + [0] * (4-len(new_row))
+
+    def _swiperow(self, row):
+        """return the row after a swipe from right to left.
+        """
+        row = self._compress(row)
+        for i in range(len(row)-1):
+            if row[i] == row[i+1]:
+                row[i] = row[i]*2
+                row[i+1] = 0
+        return self._compress(row)
+
+    #Swipe left  
+    def swipeLeft(self):
+        for i in range(4):
+            self.board[i, :] = self._swiperow(self.board[i, :])
+
+        #for i in range(board_size):
+        #    zvalue =  self.board[i, :]
+        #    nvalue = zvalue[zvalue !=0]   
+        #    fvalue = np.zeros_like(zvalue)
+        #    fvalue[:len(nvalue)] = nvalue
+        #    self.board[i, :] = fvalue
+        #    temp_board = cp.deepcopy(self.board)
 
             # merging while swiping left
-
 
     #Swipe Right  
     def swipeRight(self, move):
@@ -78,7 +97,7 @@ if __name__ == "__main__":
     ge = GameEngine()
     ge.setRandomNumberInTile(k=2)
     print(ge.printboard())
-    ge.swipeLeft(move='l')
+    ge.swipeLeft()
     print(ge.printboard())
     #print(ge.TestGoal())
     #ge.board[0][3] = 2048

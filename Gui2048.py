65b9376a979fae22f9379bdd3791f43dedd56d71
Dinesh Kumar Khadka <imkdkumar@outlook.com>
2022-03-05 19:48:25 +0100

    gui test file added

--- Gui2048.py
+++ Gui2048.py
@@ -23,7 +23,7 @@ class MyPygame:
         pygame.display.set_caption("My 2048")
 
         pygame.font.init()
-        self.myfont = pygame.font.SysFont('Comic Sans MS', 30)
+        self.primary_font = pygame.font.SysFont('Comic Sans MS', 36)
 
         self.screen = pygame.display.set_mode((self.screen_w, self.screen_h))
 
@@ -50,16 +50,10 @@ class MyPygame:
 
                 if temp == 0:
                     continue
-                text_surface = self.myfont.render(f'{temp}', True, (0, 0, 0))
-                text_rect = text_surface.get_rect(center=(top_left_x + tile_w / 2,
-                                                          top_left_y + tile_h / 2))
-                self.screen.blit(text_surface, text_rect)
+                text = self.primary_font.render(f'{temp}', True, FONT_COLOR[2])
+                text_pos = text.get_rect(center=(top_left_x + tile_w / 2, top_left_y + tile_h / 2))
+                self.screen.blit(text, text_pos)
 
-    def _swipeLeft(self, board_values) -> np.ndarray:
-        new_board = np.zeros_like(board_values)
-        for i in range(self.board_size):
-            new_board[i, :] = self._swiperow(board_values[i, :])
-        return new_board
 
     def play(self):
         self.get_random_tile_number(2)

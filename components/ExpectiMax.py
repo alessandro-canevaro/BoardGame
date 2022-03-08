1f59ce345091e478a2f7f866a00a8040a26f55a7
Alessandro Canevaro <s212977@dtu.dk>
2022-03-08 17:08:03 +0100

    Implemented depth>1 search

--- components/ExpectiMax.py
+++ components/ExpectiMax.py
@@ -1,53 +1,83 @@
 import numpy as np
 from components.Board import Board
 
+class Node():
+    def __init__(self, parent, board):
+        self.parent = parent
+        self.children = []
+        self.board = board
 
-class ExpectiMaxAgent:
-    def __init__(self) -> None:
-        self.w_matrix = np.array([[2, 2**2, 2**3, 2**4],
-        [2**8, 2**7, 2**6, 2**5],
-        [2**9, 2**10, 2**11, 2**12],
-        [2**16, 2**15, 2**14, 2**13]
-        ])
-        
+class MaxNode(Node):
+    def __init__(self, parent, board, probability):
+        super().__init__(parent, board)
+        self.prob = probability
 
-    def ExpandTree(self, board) -> dict:
-        """For each possible move generate all the possible boards with one new tile"""
-        boards_dict = {}
-        possible_moves = board.PossibleMoves()
-        for m in board.PossibleMoves():
-            moved_board = Board(board)
+    def Expand(self):
+        for m in self.board.PossibleMoves():
+            moved_board = Board(self.board)
             moved_board.Swipe(m)
-            boards_dict[m] = {2: [], 4: []}
-            for pos in moved_board.GetEmptyTiles():
-                boards_dict[m][2].append(Board(moved_board))
-                boards_dict[m][2][-1].SetEmptyTile(pos, 2)
-                boards_dict[m][4].append(Board(moved_board))
-                boards_dict[m][4][-1].SetEmptyTile(pos, 4)
+            self.children.append(ChanceNode(self, moved_board, m))
 
-        return boards_dict
+    def ComputeHeuristic(self, depth):
+        if depth == 1:
+            w_matrix = np.array([[2, 2**2, 2**3, 2**4],
+                                [2**8, 2**7, 2**6, 2**5],
+                                [2**9, 2**10, 2**11, 2**12],
+                                [2**16, 2**15, 2**14, 2**13]])
+            return int(np.sum(np.multiply(self.board.values, w_matrix)))
 
-    def ComputeHeuristics(self, board) -> int:
+        best_move = self.GetBestMove(depth=depth-1)
+        if best_move == 'game_over':
+            return 0
+        best_child = next((c for c in self.children if c.move == best_move))
+        return best_child.GetHeuristic(depth=depth-1)
+
+    def GetBestMove(self, depth):
+        if self.children == []:
+            self.Expand()
+        moves = {c.move: c.GetHeuristic(depth) for c in self.children}
+        if moves:
+            return max(moves, key=moves.get)
+        return 'game_over'
+
+class ChanceNode(Node):
+    def __init__(self, parent, board, move):
+        super().__init__(parent, board)
+        self.move = move
+
+    def Expand(self):
+        for pos in self.board.GetEmptyTiles():
+            for prob, val in zip([0.9, 0.1], [2, 4]):
+                new_board = Board(self.board)
+                new_board.SetEmptyTile(pos, val)
+                self.children.append(MaxNode(self, new_board, prob))
+
+    def GetHeuristic(self, depth):
+        if self.children == []:
+            self.Expand()
         """ referencing the paper http://cs229.stanford.edu/proj2016/report/NieHouAn-AIPlays2048-report.pdf"""
-        return int(np.sum(np.multiply(board.values, self.w_matrix)))
+        return sum([c.ComputeHeuristic(depth)*c.prob for c in self.children]) / sum([c.prob for c in self.children])
 
-    def ComputeNextMove(self, board) -> str:
-        moves_dict = self.ExpandTree(board)
-        for move, boards_dict in moves_dict.items():
-            for num, boards_list in boards_dict.items():
-                moves_dict[move][num] = np.mean([self.ComputeHeuristics(b) for b in boards_list])
-            moves_dict[move] = 0.9*moves_dict[move][2]+0.1*moves_dict[move][4]
-        return max(moves_dict, key=moves_dict.get)
+    
+class ExpectiMaxAgent:
+    def __init__(self, board) -> None:
+        self.root = MaxNode(None, board, 1)
+        
+    def ComputeNextMove(self) -> str:
+        return self.root.GetBestMove(depth=2)
+
+    def UpdateTree(self, board):
+        self.root = MaxNode(None, board, 1)
     
 
 if __name__ == "__main__":
     print("Do some tests...")
-    b = Board()
-    b.SetEmptyTile((2, 2), 2)
-    print(b)
-    agent = ExpectiMaxAgent()
-    new_board = agent.ExpandTree(b)['l'][4][0]
-    print(new_board)
-    print(agent.ComputeHeuristics(new_board))
-    print(agent.ComputeNextMove(b))
+    #b = Board()
+    #b.SetEmptyTile((2, 2), 2)
+    #print(b)
+    #agent = ExpectiMaxAgent()
+    #new_board = agent.ExpandTree(b)['l'][4][0]
+    #print(new_board)
+    #print(agent.ComputeHeuristics(new_board))
+    #print(agent.ComputeNextMove(b))
     print("All done")
--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from components.ExpectiMax import ExpectiMaxAgent
 def main():
     ge = GameEngine()
     ge.setRandomNumberInTile(k=2)
-    em = ExpectiMaxAgent()
+    em = ExpectiMaxAgent(ge.board)
     i = 0
     while i <= 1000:
         ge.printboard()
@@ -18,14 +18,15 @@ def main():
             print("Victory!")
             break
 
-        em_move = em.ComputeNextMove(ge.board)
-        print("AI suggests: {}".format(em_move))
+        em_move = em.ComputeNextMove()
+        print("{}: AI suggests: {}".format(i, em_move))
         #move = ''
         #possible_moves = ge.board.PossibleMoves()
         #while move not in possible_moves:
         #    move = input("Select your next move {}:".format(possible_moves)).lower()
         ge.board.Swipe(em_move)
         ge.setRandomNumberInTile(k=1)
+        em.UpdateTree(ge.board)
         i += 1
 
 

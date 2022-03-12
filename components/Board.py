634f519eaee77c8f3ca98351b810e61d155c51e6
HUIYULEO <S212648@DTU.DK>
2022-03-12 16:52:05 +0100

    Add the MCTSAgent

--- components/Board.py
+++ components/Board.py
@@ -1,9 +1,11 @@
 import numpy as np
 
+
 class Board:
     def __init__(self, board=None, board_size=4) -> None:
         self.board_size = board.board_size if board else board_size
         self.values = board.values.copy() if board else np.zeros((self.board_size, self.board_size))
+        self.score = 0
 
     def __repr__(self) -> str:
         return str(self.values)
@@ -18,8 +20,8 @@ class Board:
         """Return a list with the possible moves between l, r, u, d.
         """
         possible_moves = []
-        for direction in ['l', 'r', 'u', 'd']:
-            new_board = self.Swipe(direction, inplace=False)
+        for direction in ['left', 'right', 'up', 'down']:
+            new_board = self.Swipe(direction, False, inplace=False)
             if not np.array_equal(self.values, new_board):
                 possible_moves.append(direction)
         return possible_moves
@@ -39,33 +41,44 @@ class Board:
         """compress all the numbers on one side of the board.
         """
         new_row = [i for i in row if i != 0]
-        return new_row + [0] * (self.board_size-len(new_row))
+        return new_row + [0] * (self.board_size - len(new_row))
 
-    def _swiperow(self, row) -> list:
+    def _swiperow(self, row, score_update=False) -> list:
         """return the row after a swipe from right to left.
         """
         row = self._compress(row)
-        for i in range(len(row)-1):
-            if row[i] == row[i+1]:
-                row[i] = row[i]*2
-                row[i+1] = 0
+        for i in range(len(row) - 1):
+            if row[i] == row[i + 1]:
+                row[i] = row[i] * 2
+                if score_update:
+                    self.score += row[i]
+                row[i + 1] = 0
         return self._compress(row)
 
-    def _swipeLeft(self, board_values) -> np.ndarray:
+    def _swiperowEfficient(self, row, score_update):
+        new_row = row[np.nonzero(row)]
+        diff = np.diff(new_row) == 0
+        new_row[diff.nonzero()] *= 2
+        new_row[diff.nonzero()[0] + 1] = 0
+        new_row = new_row[np.nonzero(new_row)]
+        padded_array = np.zeros_like(row)
+        padded_array[:new_row.shape[0]] = new_row
+        return padded_array
+
+    def _swipeLeft(self, board_values, score_update) -> np.ndarray:
         new_board = np.zeros_like(board_values)
         for i in range(self.board_size):
-            new_board[i, :] = self._swiperow(board_values[i, :])
+            new_board[i, :] = self._swiperow(board_values[i, :], score_update)
         return new_board
-    
-        
-    def Swipe(self, direction, inplace=True): 
-        moves2rot = {'l': (0, 4),
-                     'u': (1, 3),
-                     'r': (2, 2),
-                     'd': (3, 1)}
+
+    def Swipe(self, direction, score_update, inplace=True):
+        moves2rot = {'left': (0, 4),
+                     'up': (1, 3),
+                     'right': (2, 2),
+                     'down': (3, 1)}
 
         new_board = np.rot90(self.values, moves2rot[direction][0])
-        new_board = self._swipeLeft(new_board)
+        new_board = self._swipeLeft(new_board, score_update)
         new_board = np.rot90(new_board, moves2rot[direction][1])
 
         if inplace:
